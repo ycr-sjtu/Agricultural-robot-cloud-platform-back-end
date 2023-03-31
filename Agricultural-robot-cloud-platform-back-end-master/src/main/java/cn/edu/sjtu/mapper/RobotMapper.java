97986af3562ab697c3803f0159b62c1702d5dc7d
we89 <104617066+we89@users.noreply.github.com>
2023-04-01 00:36:32 +0800

    Delete Agricultural-robot-cloud-platform-back-end-master directory

--- Agricultural-robot-cloud-platform-back-end-master/src/main/java/cn/edu/sjtu/mapper/RobotMapper.java
+++
@@ -1,9 +0,0 @@
-package cn.edu.sjtu.mapper;
-
-import cn.edu.sjtu.entity.Robot;
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RobotMapper extends BaseMapper<Robot> {
-}
